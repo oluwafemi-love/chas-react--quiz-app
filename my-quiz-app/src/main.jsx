5179833bc26cab2a32b8e0c8d0da75c19db53176
Oluwafemi Love <42425031+oluwafemi-love@users.noreply.github.com>
2025-12-27 01:59:30 +0100

    Project Setup

--- my-quiz-app/src/main.jsx
+++ my-quiz-app/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './main.css'
+// import App from './App.jsx'
+import App from './components/App/index'
+
+const uniqueId = 'content-wrapper';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App />  
   </StrictMode>,
 )
